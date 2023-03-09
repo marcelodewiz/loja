dd6b718cff76669b395dbff21c7adb41f88d9299
Marcelo Migotto <marcelo.dewiz@gmail.com>
2023-03-09 10:39:11 -0300

    criado projeto do shopping api

---
+++ shopping-api/src/test/java/br/com/escaioni/shoppingapi/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escaioni.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
