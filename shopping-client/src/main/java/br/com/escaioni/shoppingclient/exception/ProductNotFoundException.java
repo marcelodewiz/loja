45baef046802279bc08be9a644e25821d841bd34
Marcelo Migotto <marcelo.dewiz@gmail.com>
2023-03-13 18:31:40 -0300

    Criado exceptions

---
+++ shopping-client/src/main/java/br/com/escaioni/shoppingclient/exception/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package br.com.escaioni.shoppingclient.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+}
---
+++ shopping-client/src/main/java/br/com/escaioni/shoppingclient/exception/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package br.com.escaioni.shoppingclient.exception;
+
+public class UserNotFoundException extends RuntimeException{
+}
